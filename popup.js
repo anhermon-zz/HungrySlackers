48a89d7b2e83be277a949c4daa7616b7dda859f7
Angel Hermon <angel@cyberdomain.local>
2017-07-20 12:44:37 +0300

    cleanup

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 (function () {
-    "use strict";
+    'use strict';
     /*global $, chrome, console*/
 
     String.prototype.contains = function (x) {
@@ -88,7 +88,7 @@
             contentType: "application/json",
             dataType: "json",
             data: JSON.stringify(dataToSend),
-            success: callback,
+            success: callback
         }).fail(errorCallback);
 
         //$.post(url, dataToSend, callback)
@@ -207,9 +207,11 @@
                             }
                         },
                         function (err) {
-                            renderStatus('Order finalization failed.');
+                            renderStatus('Order finalization failed.', err);
 
                         }, doRefresh);
+                }, function (err) {
+                    renderStatus('Order finalization failed.', err);
                 });
             });
         });
@@ -245,250 +247,3 @@
 
     });
 }());
-
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-String.prototype.contains = function (x) {
-    return this.indexOf(x) > -1;
-}
-
-var server = 'http://172.16.50.88:9000/HungrySlackers';
-var openUrl = '/invite';
-var joinUrl = '/join';
-var closeUrl = '/finalize';
-var cancelUrl = '/cancel';
-
-
-var tenBisDishesIndexUrl = 'https://www.10bis.co.il/ShoppingCart/DishesIndex';
-var orderConfirmationUrl = 'https://www.10bis.co.il/OrderConfirmation';
-var addDishAjaxUrl = 'https://www.10bis.co.il/ShoppingCart/AddDishAjax';
-
-function doRefresh() {
-    chrome.tabs.getSelected(null, function (tab) {
-        var code = 'window.location.reload();';
-        chrome.tabs.executeScript(tab.id, {
-            code: code
-        });
-        renderStatus('Order is ready');
-    });
-}
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-    // Query filter to be passed to chrome.tabs.query - see
-    // https://developer.chrome.com/extensions/tabs#method-query
-    var queryInfo = {
-        active: true,
-        currentWindow: true
-    };
-
-    chrome.tabs.query(queryInfo, function (tabs) {
-        // chrome.tabs.query invokes the callback with a list of tabs that match the
-        // query. When the popup is opened, there is certainly a window and at least
-        // one tab, so we can safely assume that |tabs| is a non-empty array.
-        // A window can only have one active tab at a time, so the array consists of
-        // exactly one tab.
-        var tab = tabs[0];
-
-        // A tab is a plain object that provides information about the tab.
-        // See https://developer.chrome.com/extensions/tabs#type-Tab
-        var url = tab.url;
-
-        // tab.url is only available if the "activeTab" permission is declared.
-        // If you want to see the URL of other tabs (e.g. after removing active:true
-        // from |queryInfo|), then the "tabs" permission is required to see their
-        // "url" properties.
-        console.assert(typeof url == 'string', 'tab.url should be a string');
-
-        callback(url);
-    });
-
-    // Most methods of the Chrome extension APIs are asynchronous. This means that
-    // you CANNOT do something like this:
-    //
-    // var url;
-    // chrome.tabs.query(queryInfo, function(tabs) {
-    //   url = tabs[0].url;
-    // });
-    // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function sendToServer(url, dataToSend, callback, errorCallback) {
-    $.ajax({
-        url: url,
-        type: "POST",
-        contentType: "application/json",
-        dataType: "json",
-        data: JSON.stringify(dataToSend),
-        success: callback
-    }).fail(errorCallback);
-
-    //$.post(url, dataToSend, callback)
-    //.fail(errorCallback);
-}
-
-function renderStatus(statusText) {
-    console.log(statusText);
-    document.getElementById('status').textContent = statusText;
-}
-
-function onMenuBtnClick() {
-    console.log('clicked');
-    getCurrentTabUrl(function (url) {
-        //TODO
-        renderStatus('Open new order with: ' + url);
-    });
-}
-
-function addCommonData(data, res) {
-    data.UserId = res.UserId;
-    data.RestaurantId = res.Restaurant.RestaurantId;
-    data.RestaurantName = res.Restaurant.RestaurantName;
-
-    //console.log(data);
-    //console.log(JSON.stringify(data));
-}
-
-function openNewOrder() {
-    baseAction(function (url) {
-        get10BisData(function (res) {
-            var data = {
-                url: url
-            };
-            addCommonData(data, res);
-            sendToServer(server + openUrl, data, function (responseData) {
-                console.log('sent to serverdd - success');
-                window.close();
-            }, function (errorMessage) {
-                renderStatus('Failed to open new order. ', errorMessage);
-            });
-        });
-    });
-}
-
-function join() {
-    baseAction(function (url) {
-        get10BisData(function (tenBisData) {
-            getOrderConfirmation(function (orderConfirmationData) {
-
-                var data = {
-                    DishList: orderConfirmationData.DishList
-                };
-
-                addCommonData(data, tenBisData);
-                sendToServer(server + joinUrl, data, function (responseData) {
-                    console.log('sent to server - success');
-                    window.close();
-                });
-            })
-        });
-    });
-}
-
-function close() {
-    baseAction(function (url) {
-        get10BisData(function (res) {
-            var data = {};
-            addCommonData(data, res)
-            sendToServer(server + closeUrl, data, function (dishesRes) {
-                addAllDishes(dishesRes.DishList,
-                    function (data) {
-                        console.log(data);
-                        if (data == true) {
-                            window.close();
-                        } else if (data == false) {
-                            renderStatus('failed');
-                        }
-                    },
-                    function (err) {
-                        renderStatus('Failed to close order', err);
-                        console.error('error:', err);
-                    }, doRefresh);
-            }, function (err) {
-                 renderStatus('Failed to close order', err);
-            });
-        });
-    });
-}
-
-function baseAction(cb) {
-    getCurrentTabUrl(function (url) {
-        if (!url.contains('www.10bis.co.il')) {
-            renderStatus('tab is not 10bis');
-            return;
-        }
-        cb(url);
-    });
-}
-
-function cancel() {
-    baseAction(function () {
-        get10BisData(function (res) {
-            data = {};
-            addCommonData(data, res);
-            sendToServer(server + cancelUrl, data, function (response) {
-                window.close();
-            });
-
-        });
-    });
-
-}
-
-function get10BisData(cb) {
-    $.get(tenBisDishesIndexUrl, cb);
-}
-
-function getOrderConfirmation(cb) {
-    $.get(orderConfirmationUrl, cb);
-}
-
-function addAllDishes(dishes, cb, onError, onFinishedAll) {
-    console.log(dishes);
-    var requests = [];
-    $.each(dishes, function (index, dish) {
-        requests.push(addDish(dish, cb, onError));
-    });
-    $.when(requests)
-     .done(onFinishedAll)
-     .fail(onError);
-}
-
-function addDish(dish, cb, onError) {
-    dish = {dishToSubmit: dish};
-    return $.post(addDishAjaxUrl, Json.stringify);
-        .done(cb)
-        .fail(onError);
-}
-
-
-document.addEventListener('DOMContentLoaded', function () {
-
-    document.getElementById("open-btn").addEventListener("click", function () {
-        openNewOrder();
-    });
-    document.getElementById("join-btn").addEventListener("click", function () {
-        join();
-    });
-    document.getElementById("close-btn").addEventListener("click", function () {
-        close();
-    });
-    document.getElementById("cancel-btn").addEventListener("click", function () {
-        cancel();
-    });
-
-});
